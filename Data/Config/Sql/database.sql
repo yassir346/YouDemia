8e54a9e15c6dccadd81e4de2d607aae5fe9fa8f7
yassir346 <yassir.mahir98@gmail.com>
2025-01-13 14:00:55 +0100

    j'ai supprimer les classe de l'enseignant, etudiant et admin de diagramme de classe

--- Data/Config/Sql/database.sql
+++ Data/Config/Sql/database.sql
@@ -9,3 +9,23 @@ CREATE TABLE categories(
     logo VARCHAR(255)
 )ENGINE=INNODB;
 
+CREATE TABLE tags(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR (35),
+    description TEXT,
+    logo VARCHAR(255)
+)ENGINE=INNODB;
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR (35),
+    description TEXT,
+    logo VARCHAR(255)
+)ENGINE=INNODB;
+
+CREATE TABLE cours(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titre VARCHAR(50),
+    
+)
+
